3349061ea624b53f0a0064f8dd04a419814a2dd8
Dmitro Zherebko <kraken@live.ru>
2018-05-19 21:59:40 +0300

    Update chatActions.js

--- src/actions/chatActions.js
+++ src/actions/chatActions.js
@@ -3,11 +3,15 @@ import { ADD_MESSAGE, ADD_USER, MESSAGE_RECEIVED, USER_LIST } from './types';
 let nextMessageId = 0;
 let nextUserId = 0;
 
-export const addMessage = (message, author) => ({
-    type: ADD_MESSAGE,
-    id: nextMessageId++,
-    message,
-    author
+export const addMessage = (message, author) =>(dispatch)=> {
+        dispatch({
+            type: ADD_MESSAGE,
+            id: nextMessageId++,
+            message,
+            author
+        })
+        action.author = params.username
+        params.socket.send(JSON.stringify(action))
 });
 
 export const addUser = name => ({
